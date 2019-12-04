2de10fcab95766cb79de7fbc48fdfca94108f4fc
Valkyrie <gebiertasmariecris007@gmail.com>
2019-12-04 22:00:45 +0800

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SharpDevelopMVC4.Controllers
+{
+	/// <summary>
+	/// Description of StudentController.
+	/// </summary>
+	public class StudentController : Controller
+	{
+		public ActionResult Index()
+		{	
+			
+			return View();
+		}
+		
+		public ActionResult Register()
+		{
+			return View ();	
+		}
+		
+		public ActionResult Save(string firstname, string lastname, string middlename, string course, string gender, string birthday)
+		{
+			ViewBag.FirstName = firstname;
+			ViewBag.LastName = lastname;
+			ViewBag.MiddleName = middlename;
+			ViewBag.Course = course;
+			ViewBag.Birthday = birthday;
+			ViewBag.Gender = gender == "M" ? "Male" : "Female";
+			
+			
+			return View ();
+		}
+	}
+}
